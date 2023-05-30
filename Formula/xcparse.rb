1f36fd72a2a4809e47ad1f4cdbeac434d3edcaca
Tim Andersson <tim@ndersson.me>
2023-05-30 17:34:12 +0200

    Update xcparse formula for v2.3.3

--- Formula/xcparse.rb
+++ Formula/xcparse.rb
@@ -1,7 +1,7 @@
 class Xcparse < Formula
   desc "Command-line extractor of screenshots/coverage from Xcode 11+ XCResult"
   homepage "https://github.com/boerworz/xcparse"
-  url "https://github.com/boerworz/xcparse.git", :tag => "v2.3.2"
+  url "https://github.com/boerworz/xcparse.git", :tag => "v2.3.3"
   head "https://github.com/boerworz/xcparse.git"
 
   depends_on :xcode => ["11.0", :build]
